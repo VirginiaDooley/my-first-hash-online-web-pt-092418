ec6ddb9f5a93c370201e5cf55e0c56c5bcb2061d
Virginia Dooley <vcdooley@gmail.com>
2018-10-01 11:19:14 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,9 +3,9 @@ def my_hash
 end
 
 
-def shipping_manifest(your_hash)
+def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  puts the_manifest
+  return the_manifest
 end
 
 def retrieval
@@ -15,8 +15,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,14 +24,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  return shipping_manifest
 end
